d005a2ebec6acfe6f1f50562462ac759649c8f4f
Nils Faltermann <nils.faltermann@cern.ch>
2021-08-30 17:23:28 +0200

    search tweak

--- NANOProducer/test/datasets/find_datasets.sh
+++ NANOProducer/test/datasets/find_datasets.sh
@@ -15,7 +15,7 @@ find_datasets(){
 	    fi
 	    echo -e "\033[1m$i\033[0m"
 	    for y in 16 17 18; do
-		sd="RunIISummer20UL${y}MiniAOD*v2*-v*/MINIAODSIM"
+		sd="RunIISummer20UL${y}MiniAOD*v2-106X*-v*/MINIAODSIM"
 		full="$i/$sd"
 		echo -e "\033[4msearching for $sd ... \033[0m"
 		dasgoclient -query="dataset status=* dataset=$full"
