86aec6b3e791cdd10b24377627dd1b23d1394b45
matt-komm <Matthias.Komm@cern.ch>
2021-10-26 12:48:15 +0100

    fix tests

--- test/runCMSSWTest.sh
+++ test/runCMSSWTest.sh
@@ -13,8 +13,8 @@ function run_test()
     echo
     echo "==================== compiling ================"
     scram b || return 1
-    wget https://raw.githubusercontent.com/WbWbX/test-files/main/RunIISummer20UL_miniAODv2/WbjToLNu_4f_TuneCP5_13TeV-madgraph-pythia8_2016_preVFP.root .
-    cmsRun ChargeReco/NANOProducer/test/produceNANO.py inputFiles=file:test-files/RunIISummer20UL_miniAODv2/WbjToLNu_4f_TuneCP5_13TeV-madgraph-pythia8_2016_preVFP.root year=2016preVFP
+    wget -nv https://raw.githubusercontent.com/WbWbX/test-files/main/RunIISummer20UL_miniAODv2/WbjToLNu_4f_TuneCP5_13TeV-madgraph-pythia8_2016_preVFP.root 
+    cmsRun ChargeReco/NANOProducer/test/produceNANO.py inputFiles=file:WbjToLNu_4f_TuneCP5_13TeV-madgraph-pythia8_2016_preVFP.root year=2016preVFP
 }
 
 run_test
