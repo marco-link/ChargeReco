012bcd38bc3b578bf61fb566239efea42f7e03a4
matt-komm <Matthias.Komm@cern.ch>
2021-10-26 12:33:31 +0100

    fix tests

--- test/runCMSSWTest.sh
+++ test/runCMSSWTest.sh
@@ -2,53 +2,19 @@ function run_test()
 {
     cd ~
     source /cvmfs/cms.cern.ch/cmsset_default.sh
-    export SCRAM_ARCH=slc7_amd64_gcc820 || return 1
+    export SCRAM_ARCH=slc7_amd64_gcc700 || return 1
     echo
     echo "==================== setup CMSSW ==============="
-    scramv1 project CMSSW CMSSW_11_1_7 || return 1
-    cd CMSSW_11_1_7/src || return 1
+    scramv1 project CMSSW CMSSW_10_6_27 || return 1
+    cd CMSSW_10_6_27/src || return 1
     eval `scram runtime -sh` || return 1
-    mkdir -p PhysicsTools/NanoAODTools
-    rsync -r --stats /scripts/ PhysicsTools/NanoAODTools/. || return 1
+    mkdir -p ChargeReco
+    rsync -r --stats /scripts/ ChargeReco/. || return 1
     echo
     echo "==================== compiling ================"
     scram b || return 1
-    
-    echo
-    echo "==================== 2016preVFP (ntags=-1) ==============="
-    python PhysicsTools/NanoAODTools/processors/ST.py --year 2016preVFP --ntags -1 --isSignal --input=https://github.com/WbWbX/test-files/raw/main/nanox_211005/WbjToLNu_4f_2016preVFP.root . || return 1
-    echo
-    echo "==================== 2016preVFP (ntags=0) ==============="
-    python PhysicsTools/NanoAODTools/processors/ST.py --year 2016preVFP --ntags 0 --isSignal --input=https://github.com/WbWbX/test-files/raw/main/nanox_211005/WbjToLNu_4f_2016preVFP.root . || return 1
-    echo
-    echo "==================== 2016preVFP (ntags=1) ==============="
-    python PhysicsTools/NanoAODTools/processors/ST.py --year 2016preVFP --ntags 1 --isSignal --input=https://github.com/WbWbX/test-files/raw/main/nanox_211005/WbjToLNu_4f_2016preVFP.root . || return 1
-    echo
-    echo "==================== 2016preVFP (ntags=2) ==============="
-    python PhysicsTools/NanoAODTools/processors/ST.py --year 2016preVFP --ntags 2 --isSignal --input=https://github.com/WbWbX/test-files/raw/main/nanox_211005/WbjToLNu_4f_2016preVFP.root . || return 1
-    
-    echo
-    echo "==================== 2016 (ntags=0) ====================="
-    python PhysicsTools/NanoAODTools/processors/ST.py --year 2016 --ntags 0 --input=https://github.com/WbWbX/test-files/raw/main/nanox_211005/DYJetsToLL_M-50_2016.root . || return 1
-    
-    echo
-    echo "==================== 2017 (ntags=-1) ====================="
-    python PhysicsTools/NanoAODTools/processors/ST.py --year 2017 --ntags -1 --isSignal --input=https://github.com/WbWbX/test-files/raw/main/nanox_211005/WbjToLNu_4f_2017.root . || return 1
-    echo
-    echo "==================== 2017 (ntags=1) ====================="
-    python PhysicsTools/NanoAODTools/processors/ST.py --year 2017 --ntags 1 --isSignal --input=https://github.com/WbWbX/test-files/raw/main/nanox_211005/WbjToLNu_4f_2017.root . || return 1
-    echo
-    echo "==================== 2017 (ntags=2) ====================="
-    python PhysicsTools/NanoAODTools/processors/ST.py --year 2017 --ntags 2 --isSignal --input=https://github.com/WbWbX/test-files/raw/main/nanox_211005/WbjToLNu_4f_2017.root . || return 1
-    
-    echo
-    echo "==================== 2018 (ntags=-1) ====================="
-    python PhysicsTools/NanoAODTools/processors/ST.py --year 2018 --ntags -1 --input=https://github.com/WbWbX/test-files/raw/main/nanox_211005/DYJetsToLL_M-10to50_2018.root . || return 1
-    echo
-    echo "==================== 2018 (ntags=0) ====================="
-    python PhysicsTools/NanoAODTools/processors/ST.py --year 2018 --ntags 0 --input=https://github.com/WbWbX/test-files/raw/main/nanox_211005/DYJetsToLL_M-10to50_2018.root . || return 1
-    
-    echo "==================== done ====================="
+    wget https://raw.githubusercontent.com/WbWbX/test-files/main/RunIISummer20UL_miniAODv2/WbjToLNu_4f_TuneCP5_13TeV-madgraph-pythia8_2016_preVFP.root .
+    cmsRun ChargeReco/NANOProducer/test/produceNANO.py inputFiles=file:test-files/RunIISummer20UL_miniAODv2/WbjToLNu_4f_TuneCP5_13TeV-madgraph-pythia8_2016_preVFP.root year=2016preVFP
 }
 
 run_test
