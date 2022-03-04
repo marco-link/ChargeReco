85f57ba13b5ca1de04547cceb85c1f327db7ea8f
Marco Link <m.link@cern.ch>
2022-03-04 10:02:58 +0100

    fix dataset invalidation script

--- crab/invalidate.py
+++ crab/invalidate.py
@@ -6,9 +6,7 @@ import os
 with open('invalidate.txt') as datasets:
     for dataset in datasets:
         print(dataset)
-        os.system('python2 $DBS3_CLIENT_ROOT/examples/DataOpsScripts/DBS3SetDatasetStatus.py --status=INVALID --dataset="{}" --url="https://cmsweb.cern.ch/dbs/prod/phys03/DBSWriter" --recursive=False'.format(dataset))
-
-
-
-
-
+        #os.system('python2 $DBS3_CLIENT_ROOT/examples/DataOpsScripts/DBS3SetDatasetStatus.py -v --status="INVALID" --dataset="{}" --url="https://cmsweb.cern.ch/dbs/prod/phys03/DBSWriter" --recursive=False'.format(dataset))
+        command='python2 $DBS3_CLIENT_ROOT/examples/DataOpsScripts/DBS3SetDatasetStatus.py -v --dataset={} --url=https://cmsweb.cern.ch/dbs/prod/phys03/DBSWriter --status=INVALID --recursive=False'.format(dataset.rstrip())
+        #print(command)
+        os.system(command)
